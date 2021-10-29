e7d7f6c9188bac4fdb591b7888f480c1e6593b39
LongMyDu <longmydu@gmail.com>
2021-10-29 11:35:10 +0700

    insert chi tiet hoa don

--- public/viewers/insertView.js
+++ public/viewers/insertView.js
@@ -1,20 +1,31 @@
 const HtmlElements = {
     customerIdInput: document.getElementById('customer-id-input'),
     dateInput: document.getElementById('date-input'),
-    form: document.getElementById('insert-form')
+    form: document.getElementById('insert-form'),
+    insertButton: document.getElementById('insert-row-button'),
+    detailList: document.getElementById('detail-list')
 };
 
+
 const insertReceiptUrl = '/insert-receipt-post';
+let id = 2;
+
+HtmlElements.insertButton.onclick = () => {
+    insertDetail(id);
+    id += 1;
+}
 
 HtmlElements.form.addEventListener("submit", (e) => {
     // Prevent reload after submit form 
     e. preventDefault();
 
-    // Get customer id and date
+    // Get customer id and date and all detail-list
     let customer_id = HtmlElements.customerIdInput.value;
     let date = HtmlElements.dateInput.value;
+    let product_detail_list = getDetailList(); 
+    data = `customer_id=${customer_id}&date=${date}&product_detail_list=` + JSON.stringify(product_detail_list);
 
-    console.log(`Insert view: customer_id=${customer_id}&date=${date}`);
+    console.log(`Insert view: customer_id=${customer_id}&date=${date}&product_detail_list=` + JSON.stringify(product_detail_list));
 
     let request = new XMLHttpRequest();
     request.open('POST', insertReceiptUrl, true);
@@ -27,5 +38,61 @@ HtmlElements.form.addEventListener("submit", (e) => {
             alert(request.responseText);
         }
     }
-    request.send(`customer_id=${customer_id}&date=${date}`);
+    request.send(data);
 })
+
+const insertDetail = (detail_id) => {
+    html = ` <li id="detail${detail_id}" class="grid width-12col tb-margin-smaller">
+    <div class="width-3col">
+        <label>Mã sản phẩm ${detail_id}</label>
+        <input id="product${detail_id}-id-input" type="text" name= "product_id">
+    </div>
+    <div class="width-4col">
+        <label>Số lượng</label>
+        <input id="product${detail_id}-number-input" type="number" min=1 name="product_number">
+    </div>
+    <div class="width-4col">
+        <label>Giá</label>
+        <input id="product${detail_id}-price-input" type="number" min=0 name="product_price">
+    </div>
+    <div class="width-1col tb-center">
+        <button detail-id =${detail_id} class="delete-button far fa-trash-alt" type="button"></button>
+    </div>
+</li>`
+    HtmlElements.detailList.insertAdjacentHTML('beforeend', html);
+    setUpDeleteButton(detail_id);
+}
+
+const setUpDeleteButton = (detail_id) => {
+    new_delete_button = HtmlElements.detailList.querySelector(`#detail${detail_id} div button`);
+    console.log(new_delete_button);
+    new_delete_button.onclick = handleDeleteDetail;
+}
+
+const handleDeleteDetail = (e) => {
+    detail_id = e.target.getAttribute("detail-id");
+    delete_detail_node = HtmlElements.detailList.querySelector(`#detail${detail_id}`);
+    HtmlElements.detailList.removeChild(delete_detail_node);
+}
+
+const getDetailList = () => {
+    product_detail_list = []
+    for (let i = 1; i < id; i++) {
+        product_id_input = document.getElementById(`product${i}-id-input`);
+        if (!product_id_input)
+            continue;
+        product_number_input = document.getElementById(`product${i}-number-input`);
+        product_price_input = document.getElementById(`product${i}-price-input`);
+
+        product_id = product_id_input.value;
+        product_number = product_number_input.value;
+        product_price = product_price_input.value;
+        product_detail_list.push({
+            product_id: product_id,
+            product_number: product_number,
+            product_price: product_price,
+        });
+
+    }
+    return product_detail_list;
+}
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
-const sql = require("mssql");
+// const sql = require("mssql");
 
 const config = {
    user: 'sa',
@@ -22,12 +22,12 @@ app.get('/', function (req, res) {
    res.sendFile("index.html");
 })
 
-
 app.post('/insert-receipt-post', function (req, res) {
    // Prepare output in JSON format
    let response = {
       customer_id:req.body.customer_id,
-      date: req.body.date //yyyy-mm-dd
+      date: req.body.date,                      //yyyy-mm-dd
+      product_detail_list: JSON.parse(req.body.product_detail_list)
    };
    console.log(response);
    
@@ -43,35 +43,35 @@ app.post('/insert-receipt-post', function (req, res) {
 
 app.get('/api/receipt-list', function (req, res) {
    // If there is no parameters then get all HoaDon
-   var sqlQuery = 'SELECT * FROM HoaDon'
-   // TODO: handle resquest with parameter
-   if (Object.keys(req.query).length !== 0) {
-      var sqlQuery = `SELECT * FROM HoaDon
-      WHERE YEAR(NgayLap) = ${req.query.year} AND MONTH(NgayLap) = ${req.query.month}`
-   }
+   // var sqlQuery = 'SELECT * FROM HoaDon'
+   // // TODO: handle resquest with parameter
+   // if (Object.keys(req.query).length !== 0) {
+   //    var sqlQuery = `SELECT * FROM HoaDon
+   //    WHERE YEAR(NgayLap) = ${req.query.year} AND MONTH(NgayLap) = ${req.query.month}`
+   // }
 
    // TODO: get receipt_list in DB
-   const request = new sql.Request();
-   request.query(sqlQuery, (err, result) => {
-      if (err) res.status(500).send(err);
-      //console.log(result.recordset);
-      const receipt_list = result.recordset.map(elm => ({ id: elm.MaHD, customer_id: elm.MaKH, date: elm.NgayLap.toLocaleDateString(), total: elm.TongTien}));
-      // Send to res
-      res.json({receipt_list: receipt_list});
-   });
+   // const request = new sql.Request();
+   // request.query(sqlQuery, (err, result) => {
+   //    if (err) res.status(500).send(err);
+   //    //console.log(result.recordset);
+   //    const receipt_list = result.recordset.map(elm => ({ id: elm.MaHD, customer_id: elm.MaKH, date: elm.NgayLap.toLocaleDateString(), total: elm.TongTien}));
+   //    // Send to res
+   //    res.json({receipt_list: receipt_list});
+   // });
 })
 
 
-sql.connect(config, err => {
-   if (err) {
-      console.log('Failed to open a SQL Database connection.', err.stack);
-      process.exit(1);
-   }
+// sql.connect(config, err => {
+//    if (err) {
+//       console.log('Failed to open a SQL Database connection.', err.stack);
+//       process.exit(1);
+//    }
    var server = app.listen(8080, function () {
       var host = server.address().address;
       var port = server.address().port;
       
       console.log("Example app listening at http://%s:%s", host, port);
    });
-});
+// });
 
