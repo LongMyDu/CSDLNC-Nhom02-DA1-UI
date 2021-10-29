62ac77a6c5eaeefa7454eb11c1592bf1226ebb9d
dungtrieu-git <64150159+dungtrieu-git@users.noreply.github.com>
2021-10-29 16:59:14 +0700

    [BE] Handling and checking messages

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
-// const sql = require("mssql");
+const sql = require("mssql");
 
 const config = {
    user: 'sa',
@@ -22,6 +22,16 @@ app.get('/', function (req, res) {
    res.sendFile("index.html");
 })
 
+Array.prototype.pushValues = function(arr) {
+   this.push.apply(this, arr);
+};
+
+function NoneEmpty(arr) {
+   for(var i = 0; i < arr.length; i++)
+     if(arr[i] === '') return false;
+   return true;
+ }
+
 app.post('/insert-receipt-post', function (req, res) {
    // Prepare output in JSON format
    let response = {
@@ -29,15 +39,49 @@ app.post('/insert-receipt-post', function (req, res) {
       date: req.body.date,                      //yyyy-mm-dd
       product_detail_list: JSON.parse(req.body.product_detail_list)
    };
-   console.log(response);
    
    //TODO: validate response
-   
+
+   // Get customer ID and date
+   var values = Object.values(response);
+   var customer_id = values[0];
+   var date = values[1]
+
+   // Get all products
+   // product_detail_list[i] % 3 === 0: MaSP
+   // product_detail_list[i] % 3 === 1: SoLuong
+   // product_detail_list[i] % 3 === 2: Gia
+   var all_prod = Object.values(values[2])
+   const product_detail_list = [];
+   for (const prod of all_prod) {
+      product_detail_list.pushValues(Object.values(prod))
+   }
+
+   // Case 1: Insufficient information
+   if (customer_id === '' || date === '' || !NoneEmpty(product_detail_list)) {
+      res.send("Cannot insert! Some information is missing!");
+   } 
+
+   // Case 2: Wrong date (inputDate > today)
+   // Note: Wrong date format has already been checked via the front-end
+   var inputDate = new Date(date);
+   var today = new Date();
+   if (inputDate.setHours(0,0,0,0) > today.setHours(0,0,0,0)) {
+      res.send("Cannot insert! Invalid date!");
+   }
+
+   // Case 3: Wrong customer_id
+
+   // Case 4: Wrong product_id
+
+   else {
+      res.send("Sent successfully! New row has been inserted!");
+   }
 
    //TODO: link to DB and insert new row
 
    //TODO: send to client successfull message
-   res.send("Sent successfully! New row has been inserted!");
+   
 })
 
 
