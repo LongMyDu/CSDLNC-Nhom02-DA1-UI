b85aaef496d92968fdb83d438ddabafac5b0884f
LongMyDu <longmydu@gmail.com>
2021-10-29 11:36:36 +0700

    uncomment

--- server.js
+++ server.js
@@ -43,35 +43,35 @@ app.post('/insert-receipt-post', function (req, res) {
 
 app.get('/api/receipt-list', function (req, res) {
    // If there is no parameters then get all HoaDon
-   // var sqlQuery = 'SELECT * FROM HoaDon'
-   // // TODO: handle resquest with parameter
-   // if (Object.keys(req.query).length !== 0) {
-   //    var sqlQuery = `SELECT * FROM HoaDon
-   //    WHERE YEAR(NgayLap) = ${req.query.year} AND MONTH(NgayLap) = ${req.query.month}`
-   // }
+   var sqlQuery = 'SELECT * FROM HoaDon'
+   // TODO: handle resquest with parameter
+   if (Object.keys(req.query).length !== 0) {
+      var sqlQuery = `SELECT * FROM HoaDon
+      WHERE YEAR(NgayLap) = ${req.query.year} AND MONTH(NgayLap) = ${req.query.month}`
+   }
 
    // TODO: get receipt_list in DB
-   // const request = new sql.Request();
-   // request.query(sqlQuery, (err, result) => {
-   //    if (err) res.status(500).send(err);
-   //    //console.log(result.recordset);
-   //    const receipt_list = result.recordset.map(elm => ({ id: elm.MaHD, customer_id: elm.MaKH, date: elm.NgayLap.toLocaleDateString(), total: elm.TongTien}));
-   //    // Send to res
-   //    res.json({receipt_list: receipt_list});
-   // });
+   const request = new sql.Request();
+   request.query(sqlQuery, (err, result) => {
+      if (err) res.status(500).send(err);
+      //console.log(result.recordset);
+      const receipt_list = result.recordset.map(elm => ({ id: elm.MaHD, customer_id: elm.MaKH, date: elm.NgayLap.toLocaleDateString(), total: elm.TongTien}));
+      // Send to res
+      res.json({receipt_list: receipt_list});
+   });
 })
 
 
-// sql.connect(config, err => {
-//    if (err) {
-//       console.log('Failed to open a SQL Database connection.', err.stack);
-//       process.exit(1);
-//    }
+sql.connect(config, err => {
+   if (err) {
+      console.log('Failed to open a SQL Database connection.', err.stack);
+      process.exit(1);
+   }
    var server = app.listen(8080, function () {
       var host = server.address().address;
       var port = server.address().port;
       
       console.log("Example app listening at http://%s:%s", host, port);
    });
-// });
+});
 
