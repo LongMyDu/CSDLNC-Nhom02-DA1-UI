b4f341d1d15305ae2ad78502499308e8419030b4
19127302 <64116177+19127302@users.noreply.github.com>
2021-10-30 12:13:45 +0700

    server.js- insert new row
    link to DB and insert new row into table HoaDon and CT_HoaDon

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ app.post('/insert-receipt-post', function (req, res) {
    };
    
    //TODO: validate response
+   let isValid = true;
 
    // Get customer ID and date
    var values = Object.values(response);
@@ -60,6 +61,7 @@ app.post('/insert-receipt-post', function (req, res) {
    // Case 1: Insufficient information
    if (customer_id === '' || date === '' || !NoneEmpty(product_detail_list)) {
       res.send("Cannot insert! Some information is missing!");
+      isValid = false;
    } 
 
    // Case 2: Wrong date (inputDate > today)
@@ -68,20 +70,72 @@ app.post('/insert-receipt-post', function (req, res) {
    var today = new Date();
    if (inputDate.setHours(0,0,0,0) > today.setHours(0,0,0,0)) {
       res.send("Cannot insert! Invalid date!");
+      isValid = false;
    }
 
    // Case 3: Wrong customer_id
+   var sqlQuery = `SELECT * FROM KhachHang KH
+                  WHERE KH.MaKH = ${customer_id}`
+   const request = new sql.Request();
+   request.query(sqlQuery, (err, result) => {
+      if (err) res.status(500).send(err);
+      if (result.recordset.length === 0) {
+         res.send("Cannot insert! Wrong customer's ID!");
+      }
+      isValid = false;
+   });
 
    // Case 4: Wrong product_id
-
-   else {
-      res.send("Sent successfully! New row has been inserted!");
+   for (let i = 0; i < product_detail_list.length / 3; i++) {
+      var sqlQuery = `SELECT * FROM SanPham SP
+      WHERE SP.MaSP = ${product_detail_list[i*3]}`
+      const request = new sql.Request();
+      request.query(sqlQuery, (err, result) => {
+         if (err) res.status(500).send(err);
+         if (result.recordset.length === 0) {
+            res.send("Cannot insert! Wrong product's ID!");
+            isValid = false;
+         }
+      });
+      if (isValid === false) break;
    }
 
-   //TODO: link to DB and insert new row
-
-   //TODO: send to client successfull message
-   
+   //TODO: link to DB and insert new row into table HoaDon and CT_HoaDon
+   if (isValid === true) {
+      // Select the last row in table HoaDon
+      var sqlQuery_select = `SELECT TOP 1 MaHD FROM HoaDon ORDER BY MaHD DESC`
+      const request = new sql.Request();
+      request.query(sqlQuery_select, (err, result) => {
+         if (err) res.status(500).send(err);
+         // Create a new receipt ID
+         var new_receipt_id = (parseInt(Object.values(result.recordset[0]), 10) + 1).toString();
+         // Check if the database is full
+         if (new_receipt_id.length > 6) {
+            res.send("Cannot insert! Database is full");
+            isValid = false;
+         }
+         // If all conditions are met, insert into HoaDon
+         if (isValid === true) {
+            new_receipt_id = ((6 - new_receipt_id.length) * '0') + new_receipt_id;
+            var sqlQuery_insert = `INSERT INTO HoaDon VALUES (${new_receipt_id},${customer_id}, ${date}, NULL)`
+            const request = new sql.Request();
+            request.query(sqlQuery_select, (err, result) => {
+               if (err) res.status(500).send(err);
+            });
+            // Insert into CT_HoaDon
+            for (let i = 0; i < product_detail_list.length; i=i+3) {
+               var sqlQuery = `INSERT INTO CT_HoaDon VALUES (${new_receipt_id}, ${product_detail_list[i]}, ${product_detail_list[i+1]}, 
+                                                             ${product_detail_list[i+2]},0, NULL)`
+               const request = new sql.Request();
+               request.query(sqlQuery, (err, result) => {
+                  if (err) res.status(500).send(err);
+               });
+            }
+            // Send to client successfull message
+            res.send("Insert sucessfully!")
+         }
+      });
+   }
 })
 
 
